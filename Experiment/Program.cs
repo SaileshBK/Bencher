b450540e3d4280ce16693e7d92c541fa30bd3138
SaileshBK <sailesh.bk1@icloud.com>
2024-04-16 21:52:27 -0500

    feat(new): Experiment project created

--- Experiment/Program.cs
+++ Experiment/Program.cs
@@ -1,5 +1,5 @@
-﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
+using Experiment.Benchmark.Methods;
 
 namespace Experiment
 {
@@ -7,39 +7,9 @@ namespace Experiment
     {
         static void Main(string[] args)
         {
-            BenchmarkRunner.Run<BenchTest>();
+            BenchmarkRunner.Run<SubstringVsTake>();
         }
 
     }
 }
 
-[MemoryDiagnoser]
-public class BenchTest
-{
-
-    private static readonly string _inputValue = "This is a test string'";
-
-    [Benchmark]
-    public string Method1()
-    {
-        char[] inputValueChar = _inputValue.ToCharArray();
-        if (inputValueChar.Last().ToString() == "'")
-        {
-            var spit = inputValueChar.Take(inputValueChar.Length - 1);
-            return spit + " value";
-        }
-
-        return _inputValue;
-    }
-
-    [Benchmark]
-    public string Method2()
-    {
-        if (_inputValue.EndsWith("'"))
-        {
-            return $"{_inputValue.Substring(0, _inputValue.Length - 1)} value";
-        }
-
-        return _inputValue;
-    }
-}
