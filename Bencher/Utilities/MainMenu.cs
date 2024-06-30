2072a5ff7f6b33756826142d3b503cdbd113b748
SaileshBK <sailesh.bk1@icloud.com>
2024-06-29 23:11:43 -0500

    feat(new): Added console starting status

--- Bencher/Utilities/MainMenu.cs
+++ Bencher/Utilities/MainMenu.cs
@@ -6,24 +6,17 @@ namespace Bencher.Utilities;
 
 internal static class MainMenu
 {
-    private static readonly Dictionary<string, string> _currentBenchmarkTypes = new();
+    private static readonly Dictionary<string, string> _currentBenchmarkTypes = [];
     public static void ShowMenu()
     {
-        var benchmarkTypes = Assembly.GetExecutingAssembly().GetTypes()
-           .Where(t => t.GetCustomAttributes(typeof(MemoryDiagnoserAttribute), false).Length > 0);
-
-        foreach (var benchmarkType in benchmarkTypes)
-        {
-            _currentBenchmarkTypes.TryAdd(benchmarkType.Name, benchmarkType.FullName!);
-        }
-
+        // Don't really need, just brings more life on console.
+        StartBencherStatus();
         // Ask for the user's input
-        var benchmarkChoice = AnsiConsole.Prompt(
-            new SelectionPrompt<string>()
-                .Title("[green]Please Select the benchmark you want to run:[/]")
-                .PageSize(10)
-                .MoreChoicesText("[grey](Move up and down to reveal more benchmarks)[/]")
-                .AddChoices(_currentBenchmarkTypes.Keys));
+        var benchmarkChoice = AnsiConsole.Prompt(new SelectionPrompt<string>()
+            .Title("[green]Please Select the benchmark you want to run:[/]")
+            .PageSize(10)
+            .MoreChoicesText("[grey](Move up and down to reveal more benchmarks)[/]")
+            .AddChoices(_currentBenchmarkTypes.Keys));
 
         AnsiConsole.WriteLine($"Running {benchmarkChoice}...");
 
@@ -33,5 +26,31 @@ internal static class MainMenu
         }
 
         MethodInvoker.Invoke(methodToInvoke!);
+    }
+
+    private static void StartBencherStatus()
+    {
+        AnsiConsole
+            .Status()
+            .Start("Starting Bencher...", ctx =>
+            {
+                ctx.Spinner(Spinner.Known.Star);
+                ctx.SpinnerStyle(Style.Parse("green"));
+                GetAllAvailableBenchmarkTypes();
+                Thread.Sleep(2000);
+            });
+    }
+
+    private static void GetAllAvailableBenchmarkTypes()
+    {
+        var benchmarkTypes = Assembly
+            .GetExecutingAssembly()
+            .GetTypes()
+            .Where(t => t.GetCustomAttributes(typeof(MemoryDiagnoserAttribute), false).Length > 0);
+
+        foreach (var benchmarkType in benchmarkTypes)
+        {
+            _currentBenchmarkTypes.TryAdd(benchmarkType.Name, benchmarkType.FullName!);
+        }
     }
 }
