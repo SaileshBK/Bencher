d327fec94533b887682055f03798d629dd52d3a1
SaileshBK <sailesh.bk1@icloud.com>
2024-07-13 00:38:03 -0500

    chore: Housekeeping

--- Bencher/Program.cs
+++ Bencher/Program.cs
@@ -5,7 +5,7 @@ namespace Bencher;
 internal class Program
 {
     /*
-     * To run the Bencher Simply set Bencher as Startup project and run.
+     * To run the Bencher Simply set as Startup project and run.
      * Note: Run it on Release mode.
      */
     private static void Main(string[] args)
